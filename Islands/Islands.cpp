6fc6213799a60395d64b95b43d6ec0f902ddd8c2
Enzo Dufour <enzo.dufour05@gmail.com>
2026-03-20 12:10:11 +0100

    Islands exercice

--- Islands/Islands.cpp
+++ Islands/Islands.cpp
@@ -1,5 +1,5 @@
 #include <vector>
-#include <assert.h>
+#include <iostream>
 
 /*
  * To analyze satellite images, a geospatial company studies maps of remote ocean regions.
@@ -17,30 +17,66 @@
  * Implement the function count_islands which returns the total number of islands present in the map.
  */
 
+static void depth_first_search(std::vector<std::vector<char>>& grid, size_t i, size_t j)
+{
+    const size_t m = grid.size();
+    const size_t n = grid[0].size();
+
+    if (i >= m || j >= n || grid[i][j] != '1')
+        return;
+
+    grid[i][j] = '0';
+
+    if (i + 1 < m) depth_first_search(grid, i + 1, j);
+    if (i > 0) depth_first_search(grid, i - 1, j);
+    if (j + 1 < n) depth_first_search(grid, i, j + 1);
+    if (j > 0) depth_first_search(grid, i, j - 1);
+}
+
 int count_islands(std::vector<std::vector<char>>& grid)
 {
-	return -1;
+    if (grid.empty()) return 0;
+
+    const size_t m = grid.size();
+    const size_t n = grid[0].size();
+    int islands = 0;
+
+    for (size_t i = 0; i < m; i++)
+    {
+        for (size_t j = 0; j < n; j++)
+        {
+            if (grid[i][j] == '1')
+            {
+                islands++;
+                depth_first_search(grid, i, j);
+            }
+        }
+    }
+
+    return islands;
 }
 
 int main()
 {
     {
-        std::vector<std::vector<char>> grid = {
-            {'1','1','0','0'},
-            {'1','1','0','0'},
-            {'0','0','1','0'},
-            {'0','0','0','1'}
+        std::vector<std::vector<char>> firstGrid = {
+            {'1', '1', '0', '0'},
+            {'1', '1', '0', '0'},
+            {'0', '0', '1', '0'},
+            {'0', '0', '0', '1'}
         };
-        assert(count_islands(grid) == 3);
+        const int firstResult = count_islands(firstGrid);
+        std::cout << "Number of islands in firstGrid: " << firstResult << '\n';
     }
 
     {
-        std::vector<std::vector<char>> grid = {
-            {'1','1','1'},
-            {'0','1','0'},
-            {'1','1','1'}
+        std::vector<std::vector<char>> secondGrid = {
+            {'1', '1', '1'},
+            {'0', '1', '0'},
+            {'1', '1', '1'}
         };
-        assert(count_islands(grid) == 1);
+        const int secondResult = count_islands(secondGrid);
+        std::cout << "Number of islands in secondGrid: " << secondResult << '\n';
     }
 
     return 0;
